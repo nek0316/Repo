73e9f324b817192e6cd2081e26908e757e9142db
Blazetamer <sales@nitroalarms.com>
2013-10-25 18:27:35 -0400

    Added Default View 1.6.0 Popcorn

--- plugin.video.popcornflix/default.py
+++ plugin.video.popcornflix/default.py
@@ -12,6 +12,11 @@ addonPath = addon.getAddonInfo('path')
 artPath = addonPath + '/art/'
 fanartPath = addonPath + '/art/'
 
+#HOOKS
+settings = xbmcaddon.Addon(id='plugin.video.popcornflix')
+
+
+
 def CATEGORIES():
     
     addDir('[COLOR blue]New Arrivals[/COLOR]','http://popcornflix.com/New-Arrivals-movies/',1,artPath+'newarrival.png')
@@ -122,6 +127,7 @@ def addDir(name,url,mode,iconimage):
         ok=True
         liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
         liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        xbmc.executebuiltin("Container.SetViewMode(500)")
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
         return ok
         
